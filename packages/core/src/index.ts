03de8d63b4623285f790475438793bdb9c3f1981
Norbert de Langen <ndelangen@me.com>
2026-07-29 17:09:54 +0200

    Add a multiplication helper

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,10 @@ export function add(left: number, right: number): number {
   return left + right;
 }
 
+export function multiply(left: number, right: number): number {
+  return left * right;
+}
+
 export interface LabelNormalizationOptions {
   locale?: string;
 }
